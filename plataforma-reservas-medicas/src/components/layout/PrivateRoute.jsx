36b01b030e7c9ae343074bcdb1a40209e26f9445
SebasTK2008 <sebastianlopez200802@gmail.com>
2026-05-22 23:41:07 -0500

    crear pages: dashboard y patients, usePatients y Apis's

--- plataforma-reservas-medicas/src/components/layout/PrivateRoute.jsx
+++ plataforma-reservas-medicas/src/components/layout/PrivateRoute.jsx
@@ -14,12 +14,12 @@ function PrivateRoute({ children }) {
   // "replace" reemplaza la entrada en el historial
   // (así el botón "atrás" no vuelve a la página protegida)
   if (!isAuthenticated) { //RECORDAR AGREGARLE LA NEGACION PARA QUE FUNCIONE CORRECTAMENTE
-    //return <Navigate to="/login" replace />;
-    return children;
+    return <Navigate to="/login" replace />;
+    //return children;
   }
 
   // Si está autenticado, muestra el contenido normalmente
-  //return children;
+  return children;
 }
 
 export default PrivateRoute;
