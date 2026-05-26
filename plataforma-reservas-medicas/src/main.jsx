84ac993c9e6d4494832c42c71d683e891a665e66
SebasTK2008 <sebastianlopez200802@gmail.com>
2026-05-26 01:11:02 -0500

    refactorizar: archivos css y crear carpeta styles para guardar estilos reutilizables

--- plataforma-reservas-medicas/src/main.jsx
+++ plataforma-reservas-medicas/src/main.jsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'  // ← IMPORTAR
+import { AuthProvider } from './context/AuthContext'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
+import './styles/components.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
